7f4a341cfedcf39886435ee306e4688d9c265eaa
Péter Ekler <peter.ekler@aut.bme.hu>
2024-09-08 12:45:05 +0200

    Minor fix
    Minor fix in time show demos related to number conversion

--- AITHello/app/src/main/java/hu/ait/aithello/MainActivity.kt
+++ AITHello/app/src/main/java/hu/ait/aithello/MainActivity.kt
@@ -7,6 +7,7 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.snackbar.Snackbar
 import hu.ait.aithello.databinding.ActivityMainBinding
 import java.util.Date
 
@@ -23,7 +24,9 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root) // root is the LinearLayout
 
         binding.btnShow.setOnClickListener {
-            val textData = binding.etName.text.toString().toInt()
+            val textData = binding.etName.text.toString()
+
+            Snackbar.make(binding.root, "$textData", Snackbar.LENGTH_SHORT).show()
 
             binding.tvData.text =
                 "$textData ${Date(System.currentTimeMillis()).toString()}"
