5a74e28fd82a00bb762daed9a1071cdd228a2efb
Péter Ekler <peter.ekler@aut.bme.hu>
2024-09-08 12:41:10 +0200

    Class 02 demos
    Hello workd projects, time show app

---
+++ AITHello/app/src/main/java/hu/ait/aithello/MainActivity.kt
@@ -0,0 +1,41 @@
+package hu.ait.aithello
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import hu.ait.aithello.databinding.ActivityMainBinding
+import java.util.Date
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        setContentView(binding.root) // root is the LinearLayout
+
+        binding.btnShow.setOnClickListener {
+            val textData = binding.etName.text.toString().toInt()
+
+            binding.tvData.text =
+                "$textData ${Date(System.currentTimeMillis()).toString()}"
+        }
+
+
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+
+}
